6028bc4934b5789a3a2f759f6b52a75313250156
Elon Rubin <emrubi@umich.edu>
2017-05-14 14:47:50 -0400

    trying to add animations

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -18,6 +18,7 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+    print(Physics.
 	}
 
   public void SwitchLanes (Utils.Movement direction) {
